49adc6804649fd9ecba69087e0f9e7e29c94ffa5
mohammed osama <mohammedosama@ieee.org>
2020-03-28 08:11:19 +0200

    setting up group tables

---
+++ postgres/migrations/20200328080655_create_category_group.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE category_group(
+    group_id BIGSERIAL REFERENCES groups (id) ON DELETE CASCADE NOT NULL,
+    category_id BIGSERIAL REFERENCES categories (id) ON DELETE CASCADE NOT NULL
+)
